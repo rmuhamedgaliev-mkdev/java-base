f0e0cf2b74d3e27d3c47896c523d6dba794b3436
Rinat Muhamedgaliev <rinat.muhamedgaliev@gmail.com>
2020-09-03 23:55:37 +0300

    Added new gradle configuration

---
+++ src/main/java/dev/rmuhamedgaliev/App.java
@@ -0,0 +1,10 @@
+package dev.rmuhamedgaliev;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println(
+      "Run app"
+    );
+  }
+}
---
+++ src/test/java/dev/rmuhamedgaliev/AppTest.java
@@ -0,0 +1,14 @@
+package dev.rmuhamedgaliev;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+  @Test
+  public void testString() {
+    assertEquals(2, 2, "1 + 1 should equal 2");
+  }
+}
